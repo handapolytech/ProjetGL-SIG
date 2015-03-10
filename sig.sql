163c1a1396a8a36ac8f926b8e05a5e8b69a94d9f
handa <louiszhang@hotmail.fr>
2015-03-10 08:49:57 +0100

    BD

--- sig.sql
+++ sig.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Client :  127.0.0.1
--- Généré le :  Lun 09 Mars 2015 à 05:52
+-- Généré le :  Mar 10 Mars 2015 à 08:47
 -- Version du serveur :  5.6.17
 -- Version de PHP :  5.5.12
 
@@ -33,7 +33,15 @@ CREATE TABLE IF NOT EXISTS `abonnement_relation` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=8 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+
+--
+-- Contenu de la table `abonnement_relation`
+--
+
+INSERT INTO `abonnement_relation` (`id`, `id_utilisateur`, `id_source`) VALUES
+(5, 1, 9),
+(6, 1, 6);
 
 -- --------------------------------------------------------
 
@@ -54,7 +62,7 @@ CREATE TABLE IF NOT EXISTS `alerte_relation` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=12 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=13 ;
 
 --
 -- Contenu de la table `alerte_relation`
@@ -64,7 +72,8 @@ INSERT INTO `alerte_relation` (`id`, `id_utilisateur`, `id_source`, `priorite`, 
 (1, 1, 1, 15, '2015-03-07 02:20:28', 'sujet', 'type', 'statut', 'description'),
 (9, 1, 1, 0, '2015-03-09 05:29:28', '', '', '', ''),
 (10, 1, 1, 15, '2015-03-09 05:29:55', 'sujet', 'type', 'statut', 'description'),
-(11, 1, 1, 7, '2015-03-09 05:44:58', 'Source Supprimé: qdf', 'suppr', 'réussi', 'qdfqsdf');
+(11, 1, 1, 7, '2015-03-09 05:44:58', 'Source Supprimé: qdf', 'suppr', 'réussi', 'qdfqsdf'),
+(12, 1, 1, 0, '2015-02-03 00:00:00', '', '', '', '');
 
 -- --------------------------------------------------------
 
@@ -76,8 +85,17 @@ CREATE TABLE IF NOT EXISTS `blacklistage_systeme` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `id_source` int(11) NOT NULL,
   PRIMARY KEY (`id`),
+  UNIQUE KEY `id_source_2` (`id_source`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
+
+--
+-- Contenu de la table `blacklistage_systeme`
+--
+
+INSERT INTO `blacklistage_systeme` (`id`, `id_source`) VALUES
+(3, 3),
+(4, 6);
 
 -- --------------------------------------------------------
 
@@ -92,16 +110,14 @@ CREATE TABLE IF NOT EXISTS `blacklistage_utilisateur_relation` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
 
 --
 -- Contenu de la table `blacklistage_utilisateur_relation`
 --
 
 INSERT INTO `blacklistage_utilisateur_relation` (`id`, `id_utilisateur`, `id_source`) VALUES
-(1, 1, 1),
-(2, 1, 1),
-(3, 1, 1);
+(1, 1, 1);
 
 -- --------------------------------------------------------
 
@@ -120,14 +136,16 @@ CREATE TABLE IF NOT EXISTS `demande_modif_relation` (
   PRIMARY KEY (`id`),
   KEY `id_utilisateur` (`id_utilisateur`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=6 ;
 
 --
 -- Contenu de la table `demande_modif_relation`
 --
 
 INSERT INTO `demande_modif_relation` (`id`, `id_utilisateur`, `id_source`, `date`, `type`, `statut`, `description`) VALUES
-(1, 1, 1, '2015-03-07 02:26:29', 'type', 'statut', 'description');
+(1, 1, 1, '2015-03-07 02:26:29', 'type', 'en attente', 'description'),
+(2, 1, 3, '2015-03-09 21:26:14', 'qsdf', 'terminé', 'qfd'),
+(5, 1, 1, '2015-03-10 04:47:51', 'qdf', 'en attente', 'qdfqsdf');
 
 -- --------------------------------------------------------
 
@@ -145,7 +163,7 @@ CREATE TABLE IF NOT EXISTS `source` (
   `periodicite` int(11) NOT NULL,
   `description` text NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=15 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=10 ;
 
 --
 -- Contenu de la table `source`
@@ -154,7 +172,6 @@ CREATE TABLE IF NOT EXISTS `source` (
 INSERT INTO `source` (`id`, `url_fournisseur`, `titre`, `niveau`, `zone`, `projection`, `periodicite`, `description`) VALUES
 (1, 'http', 'AucuneSource', 'haut', 'France', 'vertical', 10, 'test'),
 (3, 'http:///www.cool.com', 'qdf', 'qqdf', 'qf', 'qdf', 23, 'qdfqsdf'),
-(4, 'http:///www.cool.com', 'qdf', 'qqdf', 'qf', 'qdf', 2, 'qdfqsdf'),
 (6, 'http:///www.cool.com', 'qdf', 'qqdf', 'qf', 'qdf', 23, 'qdfqsdf'),
 (9, 'http:///www.cool.com', 'qdf', 'qfd', 'qf', 'qdf', 23, 'qdfqsdf');
 
@@ -259,14 +276,15 @@ CREATE TABLE IF NOT EXISTS `theme_relation` (
   PRIMARY KEY (`id`),
   KEY `id_theme` (`id_theme`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=5 ;
 
 --
 -- Contenu de la table `theme_relation`
 --
 
 INSERT INTO `theme_relation` (`id`, `id_source`, `id_theme`) VALUES
-(3, 3, 2);
+(3, 3, 2),
+(4, 6, 1);
 
 -- --------------------------------------------------------
 
@@ -281,14 +299,15 @@ CREATE TABLE IF NOT EXISTS `utilisateur` (
   `role` varchar(255) NOT NULL,
   `alert_frequence` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
 --
 -- Contenu de la table `utilisateur`
 --
 
 INSERT INTO `utilisateur` (`id`, `nom`, `email`, `role`, `alert_frequence`) VALUES
-(1, 'inconnu', '', 'admin', 99999);
+(1, 'inconnu', 'louiszhang@hotmail.fr', 'admin', 99997),
+(2, 'dsqqfd', 'qdfqdf', 'qdfqsd', 999);
 
 -- --------------------------------------------------------
 
@@ -304,14 +323,29 @@ CREATE TABLE IF NOT EXISTS `version` (
   `date_creation_fournisseur` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `id_source` (`id_source`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=17 ;
 
 --
 -- Contenu de la table `version`
 --
 
 INSERT INTO `version` (`id`, `id_source`, `url_serveur`, `version`, `date_creation_fournisseur`) VALUES
-(1, 1, 'url_serveur', 'version', '2015-03-07 01:49:25');
+(1, 1, 'url_serveur', 'version', '2015-03-07 01:49:25'),
+(2, 3, 'okok', '1.0', '2015-03-10 06:51:49'),
+(3, 3, 'dsqsf', '2.0', '2015-03-10 06:51:49'),
+(4, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', 'v3.0', '2015-03-10 07:33:03'),
+(5, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', 'v3.0', '2015-03-10 07:33:21'),
+(6, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', 'v3.0', '2015-03-10 07:33:55'),
+(7, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', 'v3.0', '2015-03-10 07:34:24'),
+(8, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 07:51:30'),
+(9, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 07:54:00'),
+(10, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 07:58:21'),
+(11, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 07:58:34'),
+(12, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 07:59:38'),
+(13, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 08:00:34'),
+(14, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 08:02:50'),
+(15, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 08:04:14'),
+(16, 9, 'http://intranet.ifips.u-psud.fr/ii/assets/files/Stages_PeiPetET/Convention%20de%20stages%20Et%20version%20ligne1415diffuse.pdf', '5.0', '2015-03-10 08:15:45');
 
 --
 -- Contraintes pour les tables exportées
@@ -321,15 +355,15 @@ INSERT INTO `version` (`id`, `id_source`, `url_serveur`, `version`, `date_creati
 -- Contraintes pour la table `abonnement_relation`
 --
 ALTER TABLE `abonnement_relation`
-  ADD CONSTRAINT `abonnement_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `abonnement_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `abonnement_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `abonnement_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `alerte_relation`
 --
 ALTER TABLE `alerte_relation`
-  ADD CONSTRAINT `alerte_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `alerte_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`);
+  ADD CONSTRAINT `alerte_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`),
+  ADD CONSTRAINT `alerte_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `blacklistage_systeme`
@@ -341,22 +375,22 @@ ALTER TABLE `blacklistage_systeme`
 -- Contraintes pour la table `blacklistage_utilisateur_relation`
 --
 ALTER TABLE `blacklistage_utilisateur_relation`
-  ADD CONSTRAINT `blacklistage_utilisateur_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `blacklistage_utilisateur_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `blacklistage_utilisateur_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `blacklistage_utilisateur_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `demande_modif_relation`
 --
 ALTER TABLE `demande_modif_relation`
-  ADD CONSTRAINT `demande_modif_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `demande_modif_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `demande_modif_relation_ibfk_1` FOREIGN KEY (`id_utilisateur`) REFERENCES `utilisateur` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `demande_modif_relation_ibfk_2` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `theme_relation`
 --
 ALTER TABLE `theme_relation`
-  ADD CONSTRAINT `theme_relation_ibfk_2` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON DELETE CASCADE,
-  ADD CONSTRAINT `theme_relation_ibfk_1` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE;
+  ADD CONSTRAINT `theme_relation_ibfk_1` FOREIGN KEY (`id_source`) REFERENCES `source` (`id`) ON DELETE CASCADE,
+  ADD CONSTRAINT `theme_relation_ibfk_2` FOREIGN KEY (`id_theme`) REFERENCES `theme` (`id`) ON DELETE CASCADE;
 
 --
 -- Contraintes pour la table `version`
